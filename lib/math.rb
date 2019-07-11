b83434d1d7c363ca2c5156c30bf7961ae7a64bd6
Andy Choi <wooseokchoi96@gmail.com>
2019-07-11 18:23:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,7 +7,7 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  num2 / num1
+  num1 / num2
 end
 
 def multiplication(num1, num2)
@@ -15,7 +15,7 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  num2 % num1
+  num1 % num2
 end
 
 def square_root(num)
